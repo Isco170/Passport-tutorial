ca0dd223be791446fdc49d276b80fc1e47ee29e6
Francisco <franciscodamana@gmail.com>
2021-03-01 10:30:16 +0200

    Serialize users

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,6 +3,16 @@ const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
 const User = require('../models/user-model');
 
+passport.serializeUser((user, done) =>{
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) =>{
+    User.findById(id).then((user) =>{
+        done(null, user.id);
+    })
+});
+
 passport.use(
     new GoogleStrategy({
     //option for the google 
@@ -14,15 +24,16 @@ passport.use(
        User.findOne({googleid : profile.id}).then((currentUser) =>{
             if(currentUser){
                 console.log('User is: ' + currentUser);
+                done(null, currentUser)
             }else{
                 new User({
                     username: profile.displayName,
                     googleid: profile.id
                 }).save().then((newUser) =>{
                     console.log('new User created: ' + newUser);
+                    done(null, newUser);
                 })
             }
        })
     })
 )
-
